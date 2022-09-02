2bdab5be48aaee6c1d656d848b48b72fe51c5ccd
Nishanth artham <arthamnishanth123@gmail.com>
2022-09-02 16:00:10 +0530

    added dynamic data

--- routes/bookController.js
+++ routes/bookController.js
@@ -3,21 +3,25 @@ import { books } from "../models/booksModel.js";
 const app = express();
 
 export const getBooks = async (req, res) => {
-  console.log("inside getBooks");
+  console.log("raised get request");
   try {
     const myBooks = await books.find();
-    res.status(200).json(myBooks);
+    res.json(myBooks);
   } catch (error) {
     console.log("error", error);
   }
 };
+
 export const postBooks = async (req, res) => {
-  console.log("inside getBooks post ");
+  console.log("inside post method ");
   try {
+    const bname = await req.body.bname;
+    const author = await req.body.author;
+    const cost = await req.body.cost;
     const b1 = new books({
-      bname: "new book 2",
-      author: "Never dies",
-      cost: 40000,
+      bname: bname,
+      author: author,
+      cost: cost,
     });
     const myBooks = await b1.save();
     res.status(200).json(myBooks);
